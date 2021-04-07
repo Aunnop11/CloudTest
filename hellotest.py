33c76db0dc75dd06aa10a6bd320f61e5ebcfd249
Aunnop11 <79188415+Aunnop11@users.noreply.github.com>
2021-04-07 15:25:42 +0700

    Update hellotest.py

--- hellotest.py
+++ hellotest.py
@@ -1,22 +1,10 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask
+server = Flask(__name__)
 
-@app.route("/John")
-def John():
-    return "Hello John"
-
-@app.route('/')
+@server.route("/")
 def hello():
-    return render_template("home.html")
+    return "Hello Aunnop Kowong"
 
-@app.route('/Welcome/<name>')
-def Welcome_name(name):
-    return "Welcome"+name+'!'
 
-@app.route('/aboutUs')
-def aboutUs():
-    return render_template("aboutUs.html")
-
-if __name__=='__main__':
-    app.debug = True
-    app.run(host='0.0.0.0',port=8000)
+if __name__=="__main__":
+    server.run(host='0.0.0.0',port=80)
